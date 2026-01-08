f6175609e33c314d68beb072eae1134be0d17b8b
peytonpalmer23-jpg <peyton.palmer23@gmail.com>
2026-01-07 21:08:32 -0600

    Change WEATHER_API_KEY to new value
    Updated the WEATHER_API_KEY in the configuration.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   LOCATION: "Birmingham,AL",
-  WEATHER_API_KEY: "YOUR_OPENWEATHER_KEY",
+  WEATHER_API_KEY: "4af01e1ce83bbf0724a64a0eab2b9fd7",
   STOCKS: ["AAPL", "MSFT", "SPY"],
   SPORTS: {
     TEAM: "Auburn Tigers"
