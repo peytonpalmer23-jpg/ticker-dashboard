40d25d8da42e21bb92da9b2a5049d712d4286ba8
peytonpalmer23-jpg <peyton.palmer23@gmail.com>
2026-01-07 22:11:32 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   LOCATION: "Birmingham,US",
   WEATHER_API_KEY: "4af01e1ce83bbf0724a64a0eab2b9fd7",
-  STOCKS: ["SPY", "AAPL", "GOOG"],
+  STOCKS: ["SPY", "AAPL", "GOOG", "TSLA"],
   SPORTS: {
     TEAM: "Auburn Tigers"
   }
