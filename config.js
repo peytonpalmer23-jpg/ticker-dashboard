6de50593b538a94be99b2ee0f8e4aca8d2bb80e8
peytonpalmer23-jpg <peyton.palmer23@gmail.com>
2026-01-07 21:44:36 -0600

    Remove stocks from CONFIG

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   LOCATION: "Birmingham,US",
   WEATHER_API_KEY: "4af01e1ce83bbf0724a64a0eab2b9fd7",
-  STOCKS: ["SPY", "AAPL", "GOOG", "BTC", "XRP", "ETH"],
+  STOCKS: ["SPY", "AAPL", "GOOG"],
   SPORTS: {
     TEAM: "Auburn Tigers"
   }
