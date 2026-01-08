90e018ed09893ce94508a7f2b345ee1d80d99d70
peytonpalmer23-jpg <peyton.palmer23@gmail.com>
2026-01-07 21:27:36 -0600

    Update stocks list in config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   LOCATION: "Birmingham,AL",
   WEATHER_API_KEY: "4af01e1ce83bbf0724a64a0eab2b9fd7",
-  STOCKS: ["AAPL", "MSFT", "SPY"],
+  STOCKS: ["SPY", "AAPL", "GOOG", "BTC", "XRP", "ETH"],
   SPORTS: {
     TEAM: "Auburn Tigers"
   }
