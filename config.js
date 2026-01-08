9fb0366ce39e41fe6f0feb24c1777cdafb0c8f64
peytonpalmer23-jpg <peyton.palmer23@gmail.com>
2026-01-07 23:03:43 -0600

    Fix formatting in config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const CONFIG = {
   LAT: 33.5186,
   LON: -86.8104,
   WEATHER_API_KEY: "4af01e1ce83bbf0724a64a0eab2b9fd7",
-  STOCKS: ["SPY","AAPL","NVDA", "TSLA"],
+  STOCKS: ["SPY","AAPL","NVDA","TSLA"],
   SPORTS_API: {
     BASE: "https://ncaa-api.henrygd.me/openapi",
     FOOTBALL: "football/fbs",
