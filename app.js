2477fb425c6a95942a6b25a8286e4a4991c634fc
peytonpalmer23-jpg <peyton.palmer23@gmail.com>
2026-01-07 23:30:55 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-import { CONFIG, STOCK_API_KEY } from './config.js';
+/* ---------------- CONFIG ---------------- */
+const CONFIG = {
+  LOCATION: "Birmingham,AL",
+  LAT: 33.5186,
+  LON: -86.8104,
+  WEATHER_API_KEY: "4af01e1ce83bbf0724a64a0eab2b9fd7",
+  STOCKS: ["AAPL","MSFT","SPY"],
+  SPORTS_API: {
+    BASE: "https://ncaa-api.henrygd.me/openapi",
+    FOOTBALL: "football/fbs",
+    BASKETBALL: "basketball-men/d1",
+    BASEBALL: "baseball-men/d1"
+  }
+};
 
+const STOCK_API_KEY = "d5fk0u1r01qnjhocpkd0d5fk0u1r01qnjhocpkdg";
 
 /* ---------------- CLOCK ---------------- */
 function updateClock() {
