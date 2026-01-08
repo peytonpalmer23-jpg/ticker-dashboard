1cff7ce8b1a87edbd6ce26cdfc70c46ab6ced71d
peytonpalmer23-jpg <peyton.palmer23@gmail.com>
2026-01-07 21:28:56 -0600

    Update location format from 'Birmingham,AL' to 'Birmingham,US'

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  LOCATION: "Birmingham,AL",
+  LOCATION: "Birmingham,US",
   WEATHER_API_KEY: "4af01e1ce83bbf0724a64a0eab2b9fd7",
   STOCKS: ["SPY", "AAPL", "GOOG", "BTC", "XRP", "ETH"],
   SPORTS: {
