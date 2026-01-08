1d2d45382a6bb9f33ad010b0383c66b984531951
peytonpalmer23-jpg <peyton.palmer23@gmail.com>
2026-01-07 23:19:49 -0600

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,6 @@ const CONFIG = {
   LOCATION: "Birmingham, US",
   LAT: 33.5186,
   LON: -86.8104,
-  WEATHER_API_KEY: "4af01e1ce83bbf0724a64a0eab2b9fd7",
   STOCKS: ["SPY","AAPL","NVDA","TSLA"],
   SPORTS_API: {
     BASE: "https://ncaa-api.henrygd.me/openapi",
@@ -13,5 +12,5 @@ const CONFIG = {
     BASEBALL: "baseball/d1"
   }
 };
-
+const WEATHER_API_KEY = "4af01e1ce83bbf0724a64a0eab2b9fd7";
 const STOCK_API_KEY = "d5fjgcpr01qnjhocmjugd5fjgcpr01qnjhocmjv0";
